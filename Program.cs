bfef0e892cc5bbf5be08a419f8c53ccbc57f4fb1
ZhiyanovAndrey <aazhiyanov@gmail.com>
2022-01-09 17:02:13 +0300

    Реализовать  решение  задачи  с  использованием  механизма  задач продолжения

--- Program.cs
+++ Program.cs
@@ -18,30 +18,33 @@ namespace Lab_22
             Console.WriteLine("Ведите целое число - размер массива");
             int n = Convert.ToInt32(Console.ReadLine());
 
-
             Console.WriteLine("Массив из случайных чисел");
-            PrintArray(GetArray(n));
+            Func<object, int[]> func1 = new Func<object, int[]>(GetArray); //выбрали Func<object, int[]>,
+                                                                           //который принимает значение и возвращает массив
+            Task<int[]> task1 = new Task<int[]>(func1, n); //начинаем с task что бы понять как должен выглядеть делегат
+                                                           //принял значения через конструктор
+                                                           //передал делегату funk1
 
-            Console.WriteLine();
+            Action<Task<int[]>> action1 = new Action<Task<int[]>>(PrintArray);
+            Task task2 = task1.ContinueWith(action1); //принял значения через ContinueWith из task1
+                                                      //передал в делегат action1
 
-            Console.WriteLine("Отсортированный массив");
-                      
-            PrintArray(SortArray(GetArray(n)));
+            Action<Task<int[]>> action2 = new Action<Task<int[]>>(SumOfArray);
+            Task task3 = task1.ContinueWith(action2); 
 
-            Console.WriteLine();
+            Action<Task<int[]>> action3 = new Action<Task<int[]>>(MaxOfArray);
+            Task task4 = task1.ContinueWith(action3); 
 
-            SumOfArray(GetArray(n));
-            MaxOfArray(GetArray(n));
-
-
+            task1.Start(); //запускает всю цепочку вывод, ввод, сортировку 
 
             Console.ReadKey();
         }
 
-        static int[] GetArray(int n) //вместо int[] можно писать Array
+        static int[] GetArray(object a) //преобразовываем object a в int n, так требует делегат Funk
         {
-        
-        int[] arr = new int[n];
+
+            int n = (int)a;
+            int[] arr = new int[n];
             Random r = new Random();
 
             for (int i = 0; i < n; i++)
@@ -51,52 +54,37 @@ namespace Lab_22
             return arr;
         }
 
-        static int[] SortArray(int[] arr)
+        static void SumOfArray(Task<int[]> task)
         {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = 0; j < arr.Length; j++)
-                {
-                    if (arr[i] > arr[j])
-                    {
-                        int t = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = t;
-                    }
-                }
-            }
-
-            return arr;
-        }
-
-        static void SumOfArray(int[] arr)
-        {
+            int[] arr=task.Result;
             int sum = 0;
             foreach (int s in arr)
             {
                 sum += s;
             }
-            Console.WriteLine("Сумма элементов массива равна: "+sum);
+            Console.WriteLine("Сумма элементов массива равна: " + sum);
         }
 
-        static void MaxOfArray(int[] arr)
+        static void MaxOfArray(Task<int[]> task)
         {
+            int[] arr = task.Result;
             int max = 0;
             foreach (int s in arr)
             {
                 if (max < s) max = s;
-                
+
             }
             Console.WriteLine("Максимальное число в массиве равно: " + max);
         }
 
-        static void PrintArray(int[] arr)
+        static void PrintArray(Task<int[]> task)
         {
+            int[] arr = task.Result; //забираем результат из task
             foreach (int s in arr)
             {
                 Console.Write($"{s} ");
             }
+            Console.WriteLine();
         }
-
     }
 }
